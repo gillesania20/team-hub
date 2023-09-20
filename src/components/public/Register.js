1a5feb15ee0a890f5e2e5ef2e958b568f947e3ba
gillesania20 <lorenzgillesania20@gmail.com>
2023-09-20 23:18:32 +0800

    modified 'Register.js'

--- src/components/public/Register.js
+++ src/components/public/Register.js
@@ -13,7 +13,7 @@ const Register = () => {
                 :''}
             <div className='min-vh-100 d-flex justify-content-center align-items-center'>
                 <div className='shadow-lg py-5 px-3 rounded'>
-                    <h1 className='text-primary text-center mb-3'>Register</h1>
+                    <h1 className='text-primary text-center mb-3 fw-bold'>Register</h1>
                     <RegisterForm messageFunc={(input)=>setMessage(input)} messageColorFunc={(input)=>setMessageColor(input)} />
                 </div>
             </div>
